bb02d3299dc27cfda7cdf9edc38d963b5b320033
MYlindaxia <2444501319@qq.com>
2021-02-10 10:15:15 +0800

    第二天任务完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,57 @@
+import {request} from "../../request/index.js";
 Page({
   data : {
-    
+    swiperList : [],
+    catesList : [],
+    floorList : [],
+  },
+  onLoad : function(options) {
+    // wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList : result.data.message
+    //     })
+    //   }
+    // })
+    // request({
+    //   url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"
+    // }).then(result => {
+    //   this.setData({
+    //     swiperList : result.data.message
+    //   })
+    // })
+    this.getSwiperList();
+    this.getCateList();
+    this.getFloorList();
+  },
+  getSwiperList() {
+    request({
+      url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"
+    }).then(result => {
+      this.setData({
+        swiperList : result.data.message
+      })
+    })
+  },
+
+  getCateList() {
+    request({
+      url:"https://api-hmugo-web.itheima.net/api/public/v1/home/catitems"
+    }).then(result => {
+      this.setData({
+        catesList : result.data.message
+      })
+    })
+  },
+
+  getFloorList() {
+    request({
+      url:"https://api-hmugo-web.itheima.net/api/public/v1/home/floordata"
+    }).then(result => {
+      this.setData({
+        floorList : result.data.message
+      })
+    })
   }
 })
